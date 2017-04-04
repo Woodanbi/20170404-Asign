546d11fb479a5b3e18945735c98e2c315727f40c
Woodanbi <dkfleksql@naver.com>
2017-04-04 13:23:56 +0900

    두 파일 추가
    두 파일 추가

---
+++ javaOO/InitValue.java
@@ -0,0 +1,12 @@
+
+public class InitValue {
+	
+	static String name = "Java";
+	public static void main(String[] args) {
+		int year = 2016;
+		System.out.format("%s는 %d년도에 개발되었다.",name,year);
+		
+
+	}
+
+}
