993e635b0d15c18ed6738811408d102883d73302
Woodanbi <dkfleksql@naver.com>
2017-04-04 15:45:14 +0900

    3번문제
    3번문제

--- javaOO/Circle.java
+++ javaOO/Circle.java
@@ -1,1 +1,14 @@
-public class Circle;
+
+public class Circle {
+	public double radius;
+	public static double PI = 3.141592;
+	
+
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+	
+	public double getArea() {
+		return radius * radius * PI;
+	}
+}
